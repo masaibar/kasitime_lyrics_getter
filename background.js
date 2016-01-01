b8de1df11f88615ff059c0f43e1af46dce85071f
masaibar <orebatemoko@gmail.com>
2016-01-02 00:01:26 +0900

    popupにタイトルを表示できるように

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
+var title = null;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (tab.url.match(/www.kasi-time.com\/item-[0-9]*\.html$/) != null) {
 		//URLが歌詞タイムの歌詞ページだったらアイコンをアドレスバーに表示する
 		chrome.pageAction.show(tabId);
+
+		chrome.tabs.executeScript(null,
+			{"code":"document.getElementsByClassName('person_list_and_other_contents')[0].getElementsByTagName('h1')[0].innerHTML"
+			}, function(result) {
+				title = result;
+			});
 	}
 });
